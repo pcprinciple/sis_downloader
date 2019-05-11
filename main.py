d57ddeba7f748532629bea1b03c1da29b76447be
pcprinciple <43027691+pcprinciple@users.noreply.github.com>
2019-05-11 15:45:00 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,60 @@
+import tool
+while True:
+    tool.screen_show()
+
+    action_str = int(input("请选择希望执行的操作："))
+
+    print("您执行的操作是[%d]"%action_str)
+
+    if action_str==1:
+        #TODO 填写页数
+        root = "E://%s//"%input("请输入目标文件夹：")
+        tool.screen_downloader()
+        slect = int(input("\n请输入数字："))
+        if slect == 0:
+            continue
+        elif slect == 1:
+            for i in range(int(input("起始页：")), int(input("终止页:")) + 1):
+                url = 'http://68.168.16.150/forum/forumdisplay.php?fid=31&filter=type&typeid=653&page='+'%d'%i
+                list_url = tool.get_every_novel_url(url)
+                for novel_url in list_url:
+                    tool.download_txt(novel_url,root)
+        elif slect == 2:
+            for i in range(int(input("起始页：")), int(input("终止页")) + 1):
+                url = 'http://68.168.16.150/forum/forumdisplay.php?fid=31&filter=type&typeid=358&page=' + '%d'%i
+                list_url = tool.get_every_novel_url(url)
+                for novel_url in list_url:
+                    tool.download_txt(novel_url,root)
+        elif slect == 3:
+            for i in range(int(input("起始页：")), int(input("终止页")) + 1):
+                url = 'http://68.168.16.150/forum/forumdisplay.php?fid=31&filter=type&typeid=167&page=' + '%d' % i
+                list_url = tool.get_every_novel_url(url)
+                for novel_url in list_url:
+                    tool.download_txt(novel_url, root)
+        elif slect == 4:
+            for i in range(int(input("起始页：")), int(input("终止页")) + 1):
+                url = 'http://68.168.16.150/forum/forumdisplay.php?fid=31&filter=type&typeid=463&page=' + '%d' % i
+                list_url = tool.get_every_novel_url(url)
+                for novel_url in list_url:
+                    tool.download_txt(novel_url, root)
+        elif slect == 5:
+            for i in range(int(input("起始页：")), int(input("终止页")) + 1):
+                url = 'http://68.168.16.150/forum/forumdisplay.php?fid=31&filter=type&typeid=357&page=' + '%d' % i
+                list_url = tool.get_every_novel_url(url)
+                for novel_url in list_url:
+                    tool.download_txt(novel_url, root)
+        elif slect== 6:
+            for i in range(int(input("起始页：")), int(input("终止页")) + 1):
+                url = 'http://68.168.16.150/forum/forumdisplay.php?fid=31&filter=type&typeid=46&page=' + '%d' % i
+                list_url = tool.get_every_novel_url(url)
+                for novel_url in list_url:
+                    tool.download_txt(novel_url, root)
+        else:
+            print("输入错误")
+
+    elif action_str==0:
+        print("欢迎再次使用[sis爬取小说系统] V 1.0")
+        break
+
+
+
---
+++ tool.py
@@ -0,0 +1,74 @@
+import requests
+import os
+import lxml,re
+from bs4 import BeautifulSoup
+
+def screen_show():
+        """显示菜单"""
+        print("*" * 50)
+        print("欢迎使用[sis爬取小说系统] V 1.0")
+        print("")
+        print("1.爬取小说")
+        print("")
+        print("0.退出系统")
+        print("*" * 50)
+
+def screen_downloader():
+    #选择后爬取的内容
+    print("+" * 50)
+    print("欢迎使用[sis-另类区小说系统] V 1.0")
+    print("")
+    print("1.gl小说")
+    print("2.冰恋小说")
+    print("3.futa小说")
+    print("4.恋物小说")
+    print("5.秀色小说")
+    print("6.其他小说")
+    print("")
+    print("0.返回上级菜单")
+    print("+" * 50)
+
+def get_every_novel_url(url):
+    try:
+        kv={
+            'user-agent':'Chrome/10.0'
+        }
+        r = requests.get(url,headers=kv)
+        r.status_code
+        r.encoding = 'gb18030'
+        soup = BeautifulSoup(r.text,'html.parser')
+        i = []
+        #print(soup.select('span > a')) 这个可以用于搜索span下面的a标签
+        for p in soup.find_all('span',id=re.compile("thread_")):
+            i.append('http://68.168.16.150/forum/'+p.contents[0]['href'])
+        return i
+    except:
+        print("获取当前页面失败")
+
+def download_txt(url,root):
+    try:
+        kv = {
+            'user-agent': 'Chrome/10.0'
+        }
+        r = requests.get(url, headers=kv,timeout=10)
+        r.status_code
+        r.encoding = 'gb18030'
+        soup = BeautifulSoup(r.text, 'html.parser')
+        text_beat = soup.find_all('div', 't_msgfont noSelect')
+        text = BeautifulSoup(str(text_beat), 'html.parser')
+        print(soup.title.string)
+        path = root + soup.title.string + '.txt'
+        if not os.path.exists(root):
+            os.mkdir(root)
+        if not os.path.exists(path):
+            with open(path, 'w', encoding='utf-8') as f:
+                for each in text.div.text.replace('\xa0', ''):
+                    f.write(each)
+                f.close()
+            print("文件已保存")
+        else :
+            print('文件已存在')
+    except:
+        print("小说下载失败")
+
+
